49b7f376deea01e1ad2d0d091b54b4daf0a54199
aydincolak <aydincolak42@gmail.com>
2025-02-28 21:31:26 +0300

    roman numbers list

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 def int_to_roman(num):
+    roman_numbers = [
+        (1, "I"), (4, "IV"), (5, "V"), (9, "IX"), (10, "X"),
+        (40, "XL"), (50, "L"), (90, "XC"), (100, "C"),
+        (400, "CD"), (500, "D"), (900, "CM"), (1000, "M")
+    ]
+
+
+
     """
     Convert an integer to a Roman numeral.
 
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+    
