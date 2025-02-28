c7b04c88918192f0b386d51d543a6f30bd469bcf
aydincolak <aydincolak42@gmail.com>
2025-02-28 21:51:17 +0300

    while part

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ def int_to_roman(num):
         (40, "XL"), (50, "L"), (90, "XC"), (100, "C"),
         (400, "CD"), (500, "D"), (900, "CM"), (1000, "M")
     ]
+    
+    result = ""
+    while num >= roman_numbers[0]:
+        result += roman_numbers[1]
+        num -= roman_numbers[0]
+    
+    return result
+
+int_to_roman(23)
 
 
 
-    """
-    Convert an integer to a Roman numeral.
-
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+#    """
+#   Convert an integer to a Roman numeral.
+#
+#    :param num: Integer value between 1 and 3999 inclusive.
+#    :return: A string representing the Roman numeral of the integer.
+#    """
     
